29b7e5a48475e69fcd10df0f98a9c273b9fa6fd7
tanuadstia <tanu@adstia.com>
2025-01-04 00:07:20 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -13,48 +13,42 @@ const App = () => {
     successDuration: 1000,
   });
 
+  //subscribe to thapa technical for more awesome videos
+
   const startListening = () =>
     SpeechRecognition.startListening({ continuous: true, language: "en-IN" });
   const { transcript, browserSupportsSpeechRecognition } =
     useSpeechRecognition();
 
   if (!browserSupportsSpeechRecognition) {
-    return null;
+    // return null;
+    console.log("error");
   }
 
   return (
     <>
-      {!browserSupportsSpeechRecognition ? (
+      <div className="container">
+        <h2>Speech to Text Converter</h2>
+        <br />
         <p>
-          Sorry, your browser or device does not support speech recognition.
+          A React hook that converts speech from the microphone to text and
+          makes it available to your React components.
         </p>
-      ) : (
-        <div className="container">
-          <h2>Speech to Text Converter</h2>
-          <br />
-          <p>
-            A React hook that converts speech from the microphone to text and
-            makes it available to your React components.
-          </p>
 
-          <div
-            className="main-content"
-            onTouchStart={() => setTextToCopy(transcript)}
-          >
-            {transcript}
-          </div>
-
-          <div className="btn-style">
-            <button onClick={setCopied}>
-              {isCopied ? "Copied!" : "Copy to clipboard"}
-            </button>
-            <button onClick={startListening}>Start Listening</button>
-            <button onClick={SpeechRecognition.stopListening}>
-              Stop Listening
-            </button>
-          </div>
+        <div className="main-content" onClick={() => setTextToCopy(transcript)}>
+          {transcript}
         </div>
-      )}
+
+        <div className="btn-style">
+          <button onClick={setCopied}>
+            {isCopied ? "Copied!" : "Copy to clipboard"}
+          </button>
+          <button onClick={startListening}>Start Listening</button>
+          <button onClick={SpeechRecognition.stopListening}>
+            Stop Listening
+          </button>
+        </div>
+      </div>
     </>
   );
 };
