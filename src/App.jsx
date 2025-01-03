a4fcb770471377af593d82d6d0f0a64869e1cac8
tanuadstia <tanu@adstia.com>
2025-01-03 19:50:48 +0530

    hch

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import "regenerator-runtime/runtime";
 
-function App() {
-  const [count, setCount] = useState(0)
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+import useClipboard from "react-use-clipboard";
+import { useState } from "react";
+
+const App = () => {
+  const [textToCopy, setTextToCopy] = useState();
+  const [isCopied, setCopied] = useClipboard(textToCopy, {
+    successDuration: 1000,
+  });
+
+  const startListening = () =>
+    SpeechRecognition.startListening({ continuous: true, language: "en-IN" });
+  const { transcript, browserSupportsSpeechRecognition } =
+    useSpeechRecognition();
+
+  if (!browserSupportsSpeechRecognition) {
+    return null;
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <div className="container">
+        <h2>Speech to Text Converter</h2>
+        <br />
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          A React hook that converts speech from the microphone to text and
+          makes it available to your React components.
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
 
-export default App
+        <div className="main-content" onClick={() => setTextToCopy(transcript)}>
+          {transcript}
+        </div>
+
+        <div className="btn-style">
+          <button onClick={setCopied}>
+            {isCopied ? "Copied!" : "Copy to clipboard"}
+          </button>
+          <button onClick={startListening}>Start Listening</button>
+          <button onClick={SpeechRecognition.stopListening}>
+            Stop Listening
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
